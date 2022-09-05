2b8555d077e361d00d66068961106e5b10c3b563
Fedor Shendelov <shendelov@bigmir.net>
2022-09-06 05:43:48 +0800

    Глобальные переменные вынесены в отдельный файл

---
+++ src/components/utils.js
@@ -0,0 +1,9 @@
+// utils
+
+const page = document.querySelector(".page");
+const profile = page.querySelector(".profile");
+const userName = profile.querySelector(".profile__user-name");
+const userInfo = profile.querySelector(".profile__user-description");
+const gallery = page.querySelector(".gallery__items");
+
+export {page, profile, userName, userInfo, gallery};
