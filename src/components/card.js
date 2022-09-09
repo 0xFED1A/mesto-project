8aa1d751b994548c5881bf5bb57c6dca32f3521c
Fedor Shendelov <shendelov@bigmir.net>
2022-09-10 07:38:39 +0800

    Добавлена обработка лайков, при помощи информации от сервера в функция createCard()

--- src/components/card.js
+++ src/components/card.js
@@ -22,6 +22,7 @@ import { deleteCardFromServer } from "./api";
  * @param {object} cardData - object wich contains data for card creation
  * @returns {object} - created and initialized card element
  */
+
 function createCard(cardData) {
   const cardTemplate = gallery.querySelector("#gallery_template").content;
   const newCard = cardTemplate.querySelector(".gallery__item").cloneNode(true);
@@ -29,6 +30,7 @@ function createCard(cardData) {
   const newCardImage = newCard.querySelector(".gallery__image");
   newCardImage.src = cardData.link;
   newCardImage.alt = cardData.name;
+
   newCardImage.addEventListener("click", () => {
     initializePopupImage(previewPopupImage, cardData.link, cardData.name);
     initializePopupCaption(previewPopupCaption, cardData.name);
@@ -39,8 +41,22 @@ function createCard(cardData) {
   newCardCaption.textContent = cardData.name;
 
   const newCardLikeButton = newCard.querySelector(".gallery__button-like");
+  const newCardLikesCounter = newCard.querySelector(".gallery__like-counter");
+  // check if user allreayd liked this card
+  cardData.likes.some(liker => liker._id == cardData.userId) ?
+    newCardLikeButton.classList.add("gallery__button-like_active") :
+    newCardLikeButton.classList.remove("gallery__button-like_active");
+
+  newCardLikesCounter.textContent = cardData.likes.length;
   newCardLikeButton.addEventListener("click", () => {
-    newCardLikeButton.classList.toggle("gallery__button-like_active");
+    const isLiked = 
+      newCardLikeButton.classList.contains("gallery__button-like_active");
+    sendLikeInfoToServer(cardData._id, !isLiked)
+      .then(serverData => {
+        newCardLikesCounter.textContent = String(serverData.likes.length); 
+        newCardLikeButton.classList.toggle("gallery__button-like_active");
+      })
+      .catch(() => console.log("Запрос изменения состояния лайка не удался"));
   });
 
   const newCardRemoveButton = newCard.querySelector(".gallery__button-delete");
