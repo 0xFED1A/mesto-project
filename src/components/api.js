fc9c7ce53def83d77d94521289300b6e0bf031cb
Fedor Shendelov <shendelov@bigmir.net>
2022-09-10 07:29:18 +0800

    Добавлена функция получения информации о пользователе

--- src/components/api.js
+++ src/components/api.js
@@ -19,3 +19,28 @@ import {
 function generateFullURL(path) {
   return serverAddress + userCohort + path;
 }
+
+/**
+ * function retrieves user information from server and 
+ * returns promise with it
+ * @returns {object} - promise with user info
+ */
+function getUserInfoFromServer() {
+  return fetch(
+    generateFullURL(userPath),
+    {
+      method: "GET",
+      headers: {authorization: userToken},
+    }
+  )
+  .then(result => {
+    if (result.ok) {
+      return result.json();
+    } else {
+      return Promise.reject(`Ошибка: ${result.status}`);
+    }
+  }); 
+}
+export {
+  getUserInfoFromServer,
+};
