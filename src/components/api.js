5c7d549ae7e0217b414e29fdbc3071930e0f80b4
Fedor Shendelov <shendelov@bigmir.net>
2022-09-10 07:28:12 +0800

    Добавлена функция генерации URL из адреса сервера, номера когорты, и пути

--- src/components/api.js
+++ src/components/api.js
@@ -9,3 +9,13 @@ import {
   likesPath,
   avatarPath,
 } from "./utils";
+
+/**
+ * function generates full URL constructing it
+ * with serverAddress, userCohort, and passed path
+ * @param {string} path - path required for URL constructing
+ * @returns {string} - constructed path
+ */
+function generateFullURL(path) {
+  return serverAddress + userCohort + path;
+}
